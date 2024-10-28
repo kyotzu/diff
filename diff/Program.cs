3a90dcc2eda4c69f879ff974b548e6e15125eeab
kyotzu <konstantinppv12@gmail.com>
2024-10-29 00:51:37 +0500

    Добавьте файлы проекта.

---
+++ diff/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace ListVsLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\kyotzu\\Desktop\\Text1.txt";
+            List<string> lines = new List<string>(File.ReadAllLines(filePath));
+
+            List<string> list = new List<string>();
+            LinkedList<string> linkedList = new LinkedList<string>();
+
+            Stopwatch listStopwatch = Stopwatch.StartNew();
+            foreach (var line in lines)
+            {
+                list.Add(line);
+            }
+            listStopwatch.Stop();
+            Console.WriteLine($"Время вставки в List<T>: {listStopwatch.ElapsedMilliseconds} ms");
+
+            Stopwatch linkedListStopwatch = Stopwatch.StartNew();
+            foreach (var line in lines)
+            {
+                linkedList.AddLast(line);
+            }
+            linkedListStopwatch.Stop();
+            Console.WriteLine($"Время вставки в LinkedList<T>: {linkedListStopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
